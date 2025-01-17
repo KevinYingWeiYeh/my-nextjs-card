955502e05b22f85a6e416b80caf456824e33b5ba
Kevin Yeh <revinyeh@gmail.com>
2025-01-17 13:52:17 -0800

    add deply to page info

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export", // Required for static export
+  basePath: "/my-nextjs-card", // Your repository name
+  images: {
+    unoptimized: true, // Required for static export
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
